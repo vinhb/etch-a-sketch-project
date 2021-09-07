2ede9788ee46191c37e9dcf83ef71a9ce47d25f6
vinhb <vinh.bui10@gmail.com>
2021-09-07 14:20:18 -0700

    add hover effect

--- javascript.js
+++ javascript.js
@@ -2,10 +2,11 @@ function grid(n) { // creates n number of divs and appends to container
     const container = document.getElementById('container')
     for (let i=0; i<n**2; i++) {
         const div = document.createElement('div')
+        div.classList.add('grid-item')
         container.appendChild(div);
     }
 
     container.style.gridTemplateColumns = `repeat(${n}, auto)`;
 }
 
-grid(16);
+grid(16); // ask user for grid size
