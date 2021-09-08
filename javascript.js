0e935e413406b09573b4d3bbcf27e2de1f455734
vinhb <vinh.bui10@gmail.com>
2021-09-07 17:00:12 -0700

    added button and js refactor and styling

--- javascript.js
+++ javascript.js
@@ -1,12 +1,28 @@
-function grid(n) { // creates n number of divs and appends to container
-    const container = document.getElementById('container')
-    for (let i=0; i<n**2; i++) {
-        const div = document.createElement('div')
-        div.classList.add('grid-item')
+const container = document.getElementById('container');
+const button = document.getElementById('reset');
+
+function grid(n) { // creates n number of divs and appends to container   
+    for (let i=0; i<(n**2); i++) { 
+        const div = document.createElement('div');
+        div.classList.add('grid-item');
+        let containerWidth = container.getBoundingClientRect();
+        let divWidth = Math.floor(containerWidth.width/n);
+        console.log(divWidth);
+        div.style.width = divWidth;
+        div.style.height = divWidth;
         container.appendChild(div);
     }
-
-    container.style.gridTemplateColumns = `repeat(${n}, auto)`;
+    container.style.gridTemplateColumns = `repeat(${n},auto)`;
 }
 
-grid(16); // ask user for grid size
+// grid(5);
+
+// function remake() {
+//     container.replaceChildren(); // removes existing grid
+//     // input = parseInt(prompt('What size square grid would you like?', 16));
+//     console.log(input);
+
+//     grid(input); //creates new grid
+// }
+
+// button.addEventListener('click', remake());
